aecd457f998a1e8de9947f70ffcfa99b8dbae8fd
Caleb <cedinsmore63@gmail.com>
2025-09-20 19:00:38 -0400

    completed project

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodOverloading
+﻿using System;
+
+namespace MethodOverloading
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+       //int
+        public static int Add(int a, int b)
         {
-            Console.WriteLine("Hello, World!");
+            return a + b;
+        }
+
+       //decimal
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+       //string
+        public static string Add(int a, int b, bool includeText)
+        {
+            int sum = a + b;
+            if (sum == 1)
+                {
+                    return sum + " dollar";
+                }
+                else
+                {
+                    return sum + " dollars";
+                }
+        }
+        static void Main()
+        {
+            Console.WriteLine(Add(2, 3));           // 5
+            Console.WriteLine(Add(2.5m, 3.7m));     // 6.2
+            Console.WriteLine(Add(1, 0, true));     // "1 dollar"
+            Console.WriteLine(Add(2, 3, true));     // "5 dollars"
         }
     }
 }
